59469fb661337b947bc5a6ca5d4be95ab31c1a8d
fredericGelinat <fredericgelinat@gmail.com>
2022-06-09 10:02:29 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContainerComponent } from './components/container/container.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ArticleComponent } from './components/article/article.component';
+import { AsideContainerComponent } from './components/aside-container/aside-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    AsideComponent,
+    FooterComponent,
+    ContainerComponent,
+    NavComponent,
+    ArticleComponent,
+    AsideContainerComponent
   ],
   imports: [
     BrowserModule
